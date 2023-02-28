2659dc17824000c85607b064ba3e35c81000b745
Inès Ilarregui <inesilarregui07gmail.com>
2023-02-28 09:34:51 -0300

    Js advance

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-console.log('jasdoihasd');
-
-
 var visibleMenu = false;
-let menu = document.getElementById('nav');
-
-function showHideMenu() {
-    if (visibleMenu = false) {
-        menu.style.display = 'block';
+let menu = document.getElementById("nav");
+function showHideMenu(){
+    if(visibleMenu==false){//si esta oculto
+        menu.style.display = "block";
         visibleMenu = true;
-    } else {
-        menu.style.display = 'none';
-        visibleMenu = false; 
+    }
+    else{
+        menu.style.display = "none";
+        visibleMenu = false;
     }
 }
-
-// const navToggle = document.getElementById('nav-toggle');
-// const nav = document.getElementById('nav');
-
-// navToggle.addEventListener('click', () => {
-//   mainNav.classList.toggle('active'); // toggle the 'active' class
-// });
